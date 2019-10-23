0816795b8292f865dde88e0cff77c0838bf4e5d5
Wahab Shah <wahab.shah@wiesheu.de>
2019-10-23 12:16:18 -0400

    first scenario written without implementation

--- qt5projects/part1-whats_in_my_fridge/usecases/check_available_groceries/tst_check_available_groceries.cpp
+++ qt5projects/part1-whats_in_my_fridge/usecases/check_available_groceries/tst_check_available_groceries.cpp
@@ -20,15 +20,21 @@ Usecases_check_available_groceries::Usecases_check_available_groceries()
 void Usecases_check_available_groceries::one_or_more_grocery_items_available()
 {
     // Given there is a list of available grocery items
-    QFAIL("not implemented");
+    auto groceryItems = new entities::GroceryItems(this);
+    QVERIFY(groceryItems);
     // And one or more grocery items are actually available
-    QFAIL("not implemented");
+    auto groceryItemsRepoDummy = new repositories::GroceryItemsRepoDummy(groceryItems);
+    groceryItems->setRepository(groceryItemsRepoDummy);
+    QVERIFY(groceryItemsRepoDummy->count() > 0);
    // When I check available groceries
-    QFAIL("not implemented");
+    auto checkAvailableGroceries = new usecases::CheckAvailableGroceries(groceryItems, this);
+    QSignalSpy checkAvailableGroceriesSuccess(checkAvailableGroceries, &usecases::CheckAvailableGroceries::success);
+    checkAvailableGroceries->run();
+    QTRY_COMPARE_WITH_TIMEOUT(checkAvailableGroceriesSuccess.count(),1,1000);
    // Then I am given the list of available grocery items
-   QFAIL("not implemented");
+    QCOMPARE(groceryItems->list().count(), groceryItemsRepoDummy->count());
    // And the grocery items are ordered by name, ascending
-   QFAIL("not implemented");
+   QVERIFY(groceryItems->isSortedBy("name","ASC"));
 }
 
 QTEST_MAIN(Usecases_check_available_groceries)
